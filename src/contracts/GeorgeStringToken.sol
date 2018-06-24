652955cb8ae679afa208c97d3183a3f7de14622b
sakuriver <suzuerain@yahoo.co.jp>
2018-06-25 01:55:30 +0900

    コミット漏れ,,,

--- src/contracts/GeorgeStringToken.sol
+++ src/contracts/GeorgeStringToken.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.4.19;
 import "zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 
 contract GeorgeStringToken is ERC721Token {
+
+
+  event Mint(address owner, uint256 tokenId);
+
   uint256 internal nextTokenId = 0;
 
-  struct GeorgeStorage {
+  struct GeorgeStringStorage {
 
     uint256 tokenId; // 主キー
 
@@ -13,16 +17,27 @@ contract GeorgeStringToken is ERC721Token {
 
   }
 
+  /* storage */
+  mapping(uint256 => GeorgeStringStorage) georgeStrings;
 
-  constructor() public ERC721Token("GeorgeStringToken", "GWTKN") {}
+  constructor() public ERC721Token("GeorgeStringToken", "GSTKN") {}
 
-  function mint() external {
+  function mint(string _value) external {
     uint256 tokenId = nextTokenId;
+    GeorgeStringStorage memory georgeString;
+    georgeString.tokenId = tokenId;
+    georgeString.value = _value;
+    georgeStrings[tokenId] = georgeString;
     nextTokenId = nextTokenId.add(1);
     super._mint(msg.sender, tokenId);
   }
 
-  function setTokenURI(uint256 _tokenId, string _message) external onlyOwnerOf(_tokenId) {
+  function getGeorgeString(uint256 _tokenId) public view returns (string) {
+    GeorgeStringStorage memory georgeString = georgeStrings[_tokenId];
+    return (georgeString.value);
+  }
+
+  function setTokenURI(uint256 _tokenId, string _message)  external onlyOwnerOf(_tokenId) {
     super._setTokenURI(_tokenId, _message);
   }
 
@@ -30,4 +45,3 @@ contract GeorgeStringToken is ERC721Token {
     super._burn(msg.sender, _tokenId);
   }
 }
-
